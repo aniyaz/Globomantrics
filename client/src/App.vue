bff594211de4de40780984650c22c793b40bab34
Aman Niyaz <amanniyaz.waplog@hotmail.com>
2020-04-06 22:47:20 +0530

    First Commit

---
+++ client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div> -->
+  <v-app id="app">
+    <router-view />
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+
+// @import 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons';
+//this should be below @import 'vuetify/dist/vuetify.min.css'
+</script>
+
+<style>
+@import 'vuetify/dist/vuetify.min.css';
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
